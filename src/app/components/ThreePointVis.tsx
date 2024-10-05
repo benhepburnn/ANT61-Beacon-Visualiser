f3869e211a615f46970322232e00c024e37acb1a
benhepburn <ben.h@homeofpmg.com>
2024-10-05 13:27:48 +1000

    use component

--- src/app/components/ThreePointVis.tsx
+++ src/app/components/ThreePointVis.tsx
@@ -2,20 +2,14 @@
 
 import { Canvas, useLoader } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { MTLLoader, OBJLoader } from 'three-stdlib';
+import Beacon from '@/app/components/Beacon';
 
 export default function ThreePointVis() {
-  const materials = useLoader(MTLLoader, '/models/beacon.mtl');
-  const beacon = useLoader(OBJLoader, '/models/beacon.obj', (loader) => {
-    materials.preload();
-    loader.setMaterials(materials);
-  });
-
   return (
     <Canvas camera={{ position: [0, 0, 30] }} style={{ height: '100vh' }}>
-      <ambientLight color="#ffffff" intensity={0.1} />
-      <hemisphereLight color="#ffffff" skyColor="#ffffbb" groundColor="#080820" intensity={1.0} />
-      <primitive object={beacon} />
+      <ambientLight color="#9999ff" intensity={0.3} />
+      <hemisphereLight color="#ffffff" groundColor="#080820" intensity={0.8} />
+      <Beacon />
       <OrbitControls />
     </Canvas>
   );
